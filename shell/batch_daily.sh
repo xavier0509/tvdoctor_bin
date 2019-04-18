4d5961287603b368fc4366ec200ffb7e62e84f19
xavier <xiejr_work@163.com>
2019-04-18 14:46:10 +0800

    update daily shell file

--- shell/batch_daily.sh
+++ shell/batch_daily.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 #batch daily shell
-# run per one days 
+# run per one days
 
 #delete the catch photo
 cd /usr/share/nginx/html
 rm -rf catch_*.jpg
+rm -rf common_*
+rm -rf logcat_*.log
 
+cd ~
+sh tvdoctor/shell/restart.sh
 
+rm -rf /usr/share/nginx/html/upload/*
+
+cd ~
+sh tvdoctor/shell/delete_apimsg_database.sh
